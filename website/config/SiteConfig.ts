3dd2480019ae78c22cd40464b12f5939cb402c01
MeatBoyed <charlierossouw@outlook.com>
2024-08-06 12:01:27 +0200

    feat: Component & Doc site added

---
+++ website/config/SiteConfig.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  name: "NextPayFast a PayFast Integration for Next.js",
+  author: "Charles Rossouw",
+  description: `A customizable, accessible, payment gateway build with ShadCN UI, using PayFast for South Africa, and Namibia.`,
+  ogImage: "https://emblor.jaleelbennett.com/og.jpg",
+  url: "https://nextpayfast.nerfdesigns.com",
+  links: {
+    twitter: "https://twitter.com/meatboyed",
+    instagram: "https://www.instagram.com/charlesrossouww/",
+    github: "https://github.com/MeatBoyed/nextpayfast",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
